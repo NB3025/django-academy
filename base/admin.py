9a8708b9788435a291ded373392f04ae47522c93
NB <over3025@gmail.com>
2021-10-28 23:33:20 +0900

    Create model Teacher

--- base/admin.py
+++ base/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Academy
+from .models import Academy, Teacher
 
 admin.site.register(Academy)
+admin.site.register(Teacher)
--- base/models.py
+++ base/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Academy(models.Model):
-    # 1	학원이름    홍길동	경기도 시시시 구구구	000-0000-0000
+    # 1	학원이름 	경기도 시시시 구구구	000-0000-0000
     # academyId = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     # academyOwner = models.CharField(max_length=30)
@@ -13,6 +13,19 @@ class Academy(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     # academyImageName = models.CharField(max_length=30)
     # customerId = models.ForeignKey(tb_customer, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
+class Teacher(models.Model):
+    # 2	홍길동	코딩	010-0101-0101
+    academy = models.ForeignKey(Academy, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    telephone = models.CharField(max_length=30)
+    subject = models.CharField(max_length=30)
+    # class = models.CharField(max_length=30)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.name
