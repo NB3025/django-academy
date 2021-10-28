733d83ff54856093e5ae847724fa217037f49ac6
Nambong Ha <over3025@gmail.com>
2021-10-28 18:12:49 +0900

    add {% for %}

--- base/views.py
+++ base/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+rooms = [
+    {'id':1, 'name':'Lets learn python!'},
+    {'id':2, 'name':'Hello django!'}
+]
 
 def index(request):
-    return render(request,'home.html')
+    context = {'rooms':rooms}
+    return render(request,'home.html',context)
