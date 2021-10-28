d91f852e409a05d99c60911cbccdce458218b2ab
NB <over3025@gmail.com>
2021-10-28 23:14:55 +0900

    modify index --> home

--- base/views.py
+++ base/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Academy
 
 rooms = [
     {'id':1, 'name':'Lets learn python!'},
     {'id':2, 'name':'Hello django!'}
 ]
 
-def index(request):
+def home(request):
     context = {'rooms':rooms}
     return render(request,'base/home.html',context)
 
