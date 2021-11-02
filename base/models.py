6ea20412de54eb910a472a48e3c8686c05d46a1f
Nambong Ha <over3025@gmail.com>
2021-11-02 20:00:50 +0900

    Create model Student

--- base/models.py
+++ base/models.py
@@ -29,3 +29,23 @@ class Teacher(models.Model):
     
     def __str__(self):
         return self.name
+
+class tb_student(models.Model):
+    # 1 홍길동 경안고 3 010 - 1234 - 1234
+    # studentId = models.PositiveIntegerField()
+    academy = models.ForeignKey(Academy, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    # 등록날짜
+    # studentReg = models.CharField(max_length=12)
+    # studentRegYYMM = models.CharField(max_length=12)
+    # studentType = models.CharField(max_length=10)
+    school = models.CharField(max_length=30)
+    grade = models.CharField(max_length=30)
+    telephone = models.CharField(max_length=30)
+    # studentHasClass = models.SmallIntegerField(default=0)
+    # studentFather = models.CharField(max_length=10, null=True)
+    # studentMother = models.CharField(max_length=10, null=True)
+    # studentFTel = models.CharField(max_length=30, null=True)
+    # studentMTel = models.CharField(max_length=30, null=True)
+    comment = models.CharField(max_length=100, null=True)
+    # customerId = models.ForeignKey(tb_customer, on_delete=models.CASCADE)
